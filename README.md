b9519cd90229016de341c08cf543a6b10fe85c22
Danish <danish@danish-Dell-System-XPS-L502X.(none)>
2014-09-03 15:05:22 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+An initiative to improve the coding culture in BITS Pilani.
+We will discuss various classical algorithms and a lot of problems from different Online Judges.
+
+This repository is meant to be used by students, to understand/refer the solutions of the problem sets. 
---
+++ template.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <algorithm> // binsearch,max(a,b),min(a,b)
+#include <math.h>
+#include <queue>
+
+#include <vector>
+#include <set>
+#include <list>
+#include <map>
+#include <string.h> // memset
+#include <cstdlib> // abs(int),labs(int),llabs(int),min,max
+#include <limits.h> // int_max,int_min,long_long_max,long_long_min
+using namespace std;
+
+int main() {
+	return 0;
+}
